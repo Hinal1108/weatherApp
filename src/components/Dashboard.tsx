3b0cf6436366f4d6d9044764ae0cc1a66eda0251
Rahul Deo Burman <burman@Rahuls-MacBook-Pro.local>
2023-11-04 16:29:52 +0530

    design added for left sidebar

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -10,6 +10,38 @@ interface DashboardState {
     location: string
 }
 
+interface IPlacesData {
+    locationName: string;
+    currentTime: string;
+    temperature: number;
+    forecast: string;
+    highTemp: number;
+    lowTemp: number;
+}
+
+const placesData: IPlacesData[] = [{
+    locationName: "Hyderabad",
+    currentTime: "16:20",
+    temperature: 29,
+    forecast: "Mostly Coludy",
+    highTemp: 30,
+    lowTemp: 21
+},{
+    locationName: "Hyderabad",
+    currentTime: "16:20",
+    temperature: 29,
+    forecast: "Mostly Coludy",
+    highTemp: 30,
+    lowTemp: 21
+},{
+    locationName: "Hyderabad",
+    currentTime: "16:20",
+    temperature: 29,
+    forecast: "Mostly Coludy",
+    highTemp: 30,
+    lowTemp: 21
+}]
+
 class Dashboard extends React.Component<DashboardProps, DashboardState> {
     constructor(props: DashboardProps) {
         super(props);
@@ -20,23 +52,41 @@ class Dashboard extends React.Component<DashboardProps, DashboardState> {
     componentDidMount(): void {
         // this.getWeatherData();
     }
-    getWeatherData = async (location:string) => {
+    getWeatherData = async (location: string) => {
         const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=6e785234f113443582465107222504&q=${location}&aqi=no`);
         const movies = await response.json();
         console.log(movies);
     }
     render() {
+        let favourites = placesData.map((place: IPlacesData) => {
+            return <div className="favouritePlace">
+                <div className="leftFav">
+                    <div>{place.locationName}</div>
+                    <div>{place.currentTime}</div>
+                    <div>{place.forecast}</div>
+                </div>
+                <div className="rightFav">
+                    <div>{place.temperature}</div>
+                    <div>{place.highTemp}</div>
+                    <div>{place.lowTemp}</div>
+                </div>
+            </div>
+        })
         return (<div className="dashboard">
 
             <div className="leftPanel">
-                <SearchBox placeholder="Search" onChanged={(newValue: string) => { this.setState({ location: newValue }) }} />
-                <PrimaryButton text="Primary" onClick={() => {
-                    console.log(this.state.location);
-                    this.getWeatherData(this.state.location);
-                }} />
-
+                {favourites}
             </div>
-            <div className="rightPanel">right</div>
+            <div className="rightPanel">
+                <div></div>
+                <div className="searchBar">
+                    <SearchBox placeholder="Search" onChanged={(newValue: string) => { this.setState({ location: newValue }) }} className="searchField" />
+                    <PrimaryButton text="Search" onClick={() => {
+                        console.log(this.state.location);
+                        this.getWeatherData(this.state.location);
+                    }} />
+                </div>
+            </div>
 
         </div>);
     }
